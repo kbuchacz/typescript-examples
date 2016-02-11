54704764dbf647ad7e545e2b62f75471c2d7d654
Krzysztof Buchacz <k.buchacz@gmail.com>
2016-02-11 15:08:33 +0000

    Wheel adjustments

--- app/src/classes/Wheel.ts
+++ app/src/classes/Wheel.ts
@@ -4,14 +4,14 @@ import {Rim} from "./Rim";
 import {Tyre} from "./Tyre";
 
 export class Wheel extends Part implements Item {
-  private rim: Rim;
-  private tyre: Tyre;
-  private model: string;
+  private rim:Rim;
+  private tyre:Tyre;
+  private model:string;
 
   itemNumber:number;
   itemDescription:string;
 
-    constructor(model: string, manufacturer: string) {
+  constructor(model:string, manufacturer:string) {
     super(manufacturer);
     this.model = model;
   }
